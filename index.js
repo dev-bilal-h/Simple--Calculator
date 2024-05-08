a62993b8f005b17ad5313c002b2d8609cef2681a
bilal <siddiquibilal882@gmail.com>
2024-05-08 16:52:50 -0700

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green("       WELCOME TO B23 SIMPLE CALCULATOR       "));
+const answer = await inquirer.prompt([
+    { message: chalk.cyanBright("Enter first number"), type: "number", name: "firstNumber" },
+    { message: chalk.cyanBright("Enter second number"), type: "number", name: "secondNumber" },
+    {
+        message: chalk.magentaBright("Select one of the operators to perform operation"),
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+// Conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
